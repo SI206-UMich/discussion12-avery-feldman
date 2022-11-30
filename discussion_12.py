1248bbed38c49b56a7ed18947c7144846424fcc6
Avery Feldman <averylf@umich.edu>
2022-11-30 17:22:03 -0500

    task1

--- discussion_12.py
+++ discussion_12.py
@@ -16,6 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER)")
+    conn.commit()
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -27,6 +29,16 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
+    js_data = json.loads(file_data)
+    for item in js_data:
+        employee_id = int(item["employee_id"])
+        first_name = item["first_name"]
+        last_name = item["last_name"]
+        hire_date = item["hire_date"]
+        job_id = int(item["job_id"])
+        salary = int(item["salary"])
+        cur.execute("INSERT OR IGNORE INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)",(employee_id,first_name,last_name,hire_date,job_id,salary))
+        conn.commit()
     pass
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
